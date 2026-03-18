627bc20e703e86cfde32e53aa53e8818b27b237f
Aaron Bustos <105888522+AJBustos28@users.noreply.github.com>
2026-03-18 16:37:52 -0600

    Ajustes Reporte

--- ToolshopDemoAUTO/Test/BaseTest/BaseTest.cs
+++ ToolshopDemoAUTO/Test/BaseTest/BaseTest.cs
@@ -1,36 +1,39 @@
 ﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using ToolshopDemoAUTO.Genericos.DriverConfig;
-using AventStack.ExtentReports.Reporter;
+using ToolshopDemoAUTO.Reportes;
 
 namespace ToolshopDemoAUTO.Test.BaseTest
 {
     public abstract class BaseTest
     {
-        
         protected IWebDriver Driver;
-        public BaseTest() { }
 
         [SetUp]
         public void Setup()
         {
+            ExtentReportManager.CreateTest(TestContext.CurrentContext.Test.Name);
+
             var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
-
             Driver = ChromeFactory.CreateDriver(options);
         }
+
         [TearDown]
         public void TearDown()
         {
-            try
-            {
-                Driver.Dispose();
-            }
-            catch (Exception ex01)
-            {
+            var result = TestContext.CurrentContext.Result;
 
+            if (result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                ExtentReportManager.LogFail(result.Message);
+                ExtentReportManager.LogScreenshot(Driver, "Error");
             }
+            ExtentReportManager.Flush();
+            Driver.Dispose();
         }
     }
 }
